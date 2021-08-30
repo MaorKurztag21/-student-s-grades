a3bf1492aac0ab0e72a45daa6e94438614f6af67
MaorKurztag21 <rnrwznr@gmail.com>
2021-08-31 02:27:18 +0300

    Half range
    And a decent output

--- student.js
+++ student.js
@@ -1,17 +1,19 @@
 
 function range(arr){
+    
     let max=Math.max.apply(Math,arr);
     let min=Math.min.apply(Math,arr);
     let result=max-min;
+    console.log(result);
 }
 
 function Median(arr){
     let sum;
     let long=arr.length -1;
-    let short=Math.floor((arr.length)/2);
+    let short=arr.length/2;
     arr.sort(function(a, b){return a - b});
    if(arr.length%2==0){
-       sum=[arr[short] + arr[short+1]]/2;
+       sum=(arr[short]+arr[short-1])/2;
    }
    if(arr.length%2==1){
     sum=arr[long/2];
@@ -19,5 +21,14 @@ function Median(arr){
    console.log(sum);
 }
 
-    
-   
+function second(arr){
+    let arr=[73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
+    let arr1=arr.slice((arr.length/2),arr.length);
+    let max1=Math.max.apply(Math,arr1);
+    let min1=Math.min.apply(Math,arr1);
+    let rang=max1-min1;
+console.log(rang);
+}
+
+
+   console.log("stats:\n\tRange:" + range([73, 91, 88, 56, 62, 84, 95, 96, 88, 92]) + "\n\tMedian:" + Median([73, 91, 88, 56, 62, 84, 95, 96, 88, 92]) +"\n\tHalf range:" + second([73, 91, 88, 56, 62, 84, 95, 96, 88, 92]));
