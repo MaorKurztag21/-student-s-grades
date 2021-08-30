eff658a37371d71a8fde02da03eb84a6de9d9fa7
MaorKurztag21 <rnrwznr@gmail.com>
2021-08-31 01:39:17 +0300

    The median grade
    Mission 2

--- student.js
+++ student.js
@@ -4,3 +4,20 @@ function range(arr){
     let min=Math.min.apply(Math,arr);
     let result=max-min;
 }
+
+function Median(arr){
+    let sum;
+    let long=arr.length -1;
+    let short=Math.floor((arr.length)/2);
+    arr.sort(function(a, b){return a - b});
+   if(arr.length%2==0){
+       sum=[arr[short] + arr[short+1]]/2;
+   }
+   if(arr.length%2==1){
+    sum=arr[long/2];
+   }
+   console.log(sum);
+}
+
+    
+   
